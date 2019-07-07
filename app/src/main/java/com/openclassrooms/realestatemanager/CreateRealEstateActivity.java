517a4375ca81eed4004d3a7bcabaaac9e8b6735e
Eliza13210 <elisabetboulanger@gmail.com>
2019-07-07 09:50:54 +0200

    Fixed layout for adding real estate activity

--- app/src/main/java/com/openclassrooms/realestatemanager/CreateRealEstateActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/CreateRealEstateActivity.java
@@ -1,46 +1,51 @@
 package com.openclassrooms.realestatemanager;
 
 import android.animation.Animator;
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.view.ViewTreeObserver;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
+import android.widget.Spinner;
 
+import java.util.Objects;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class CreateRealEstateActivity extends Activity {
+public class CreateRealEstateActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @BindView(R.id.root_layout)
     FrameLayout rootLayout;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       // overridePendingTransition(R.anim.do_not_move, R.anim.do_not_move);
+        // overridePendingTransition(R.anim.do_not_move, R.anim.do_not_move);
         setContentView(R.layout.activity_create_real_estate);
-
         ButterKnife.bind(this);
+        initAnimation(savedInstanceState);
+        initSpinner();
+    }
 
-        Log.e("fab", "created");
+    private void initAnimation(Bundle savedInstanceState) {
         if (savedInstanceState == null) {
-
-            Log.e("circ", "view invisible");
             rootLayout.setVisibility(View.INVISIBLE);
 
             ViewTreeObserver viewTreeObserver = rootLayout.getViewTreeObserver();
             if (viewTreeObserver.isAlive()) {
-
-                Log.e("circ", "alive");
                 viewTreeObserver.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
                     @Override
                     public void onGlobalLayout() {
 
-                        Log.e("circ", "on global called");
                         circularRevealActivity();
                         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
                             rootLayout.getViewTreeObserver().removeGlobalOnLayoutListener(this);
@@ -53,25 +58,62 @@ public class CreateRealEstateActivity extends Activity {
         }
     }
 
+    private void circularRevealActivity() {
 
-    private void circularRevealActivity(){
+        int cx = rootLayout.getWidth() / 2;
+        int cy = 56;
 
-        Log.e("circ", "circular activity called");
+        float finalRadius = Math.max(rootLayout.getWidth(), rootLayout.getHeight());
 
-            int cx = rootLayout.getWidth() / 2;
-            int cy = rootLayout.getHeight() / 2;
-
-            float finalRadius = Math.max(rootLayout.getWidth(), rootLayout.getHeight());
-
-            // create the animator for this view (the start radius is zero)
+        // create the animator for this view (the start radius is zero)
         Animator circularReveal = null;
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
             circularReveal = ViewAnimationUtils.createCircularReveal(rootLayout, cx, cy, 0, finalRadius);
         }
         circularReveal.setDuration(1000);
 
-            // make the view visible and start the animation
-            rootLayout.setVisibility(View.VISIBLE);
-            circularReveal.start();
+        // make the view visible and start the animation
+        rootLayout.setVisibility(View.VISIBLE);
+        circularReveal.start();
+        initToolbar();
+    }
+
+    protected void initToolbar() {
+        //Initiate toolbar to navigate back
+        this.setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(v -> startActivity(new Intent(this, MainActivity.class)));
+    }
+
+
+    private void initSpinner(){
+        //Rooms spinner
+        Spinner spinner = (Spinner) findViewById(R.id.spinner_rooms);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.rooms_array, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+        //Bathrooms spinner
+         Spinner spinnerBathroom = (Spinner) findViewById(R.id.spinner_bathrooms);
+        ArrayAdapter<CharSequence> adapterBathroom = ArrayAdapter.createFromResource(this,
+                R.array.bathrooms_array, android.R.layout.simple_spinner_item);
+
+        adapterBathroom.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerBathroom.setAdapter(adapter);
+        spinnerBathroom.setOnItemSelectedListener(this);
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
     }
 }
+
