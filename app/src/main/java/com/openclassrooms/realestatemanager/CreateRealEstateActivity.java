29b08ca271db72ffde1753ff3f40a4a01b96a34b
Eliza13210 <elisabetboulanger@gmail.com>
2019-07-07 10:31:18 +0200

    real estate activity starting to fetch ui info

--- app/src/main/java/com/openclassrooms/realestatemanager/CreateRealEstateActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/CreateRealEstateActivity.java
@@ -9,9 +9,17 @@ import android.view.ViewAnimationUtils;
 import android.view.ViewTreeObserver;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Switch;
 
+import com.openclassrooms.realestatemanager.models.Photo;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,6 +33,56 @@ public class CreateRealEstateActivity extends AppCompatActivity implements Adapt
     FrameLayout rootLayout;
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+    @BindView(R.id.agent)
+    EditText agent_tv;
+    //TYPE
+    @BindView(R.id.house)
+    ImageView house;
+    @BindView(R.id.apartement)
+    ImageView apartement;
+    @BindView(R.id.type)
+    EditText type_tv;
+
+    @BindView(R.id.surface)
+    EditText surface_tv;
+    @BindView(R.id.price)
+    EditText price_tv;
+    //SPINNERS
+    @BindView(R.id.spinner_rooms)
+    Spinner spinnerRooms;
+    @BindView(R.id.spinner_bathrooms)
+    Spinner spinnerBathrooms;
+
+    @BindView(R.id.description)
+    EditText description_tv;
+
+    //LOCATION
+    @BindView(R.id.geo_loc)
+    ImageView geoLocation;
+    @BindView(R.id.location)
+    EditText location_tv;
+    @BindView(R.id.address)
+    EditText address_tv;
+
+    //BUTTON
+    @BindView(R.id.btn_send)
+    Button btn_validate;
+
+    // For creating real estate object
+    private List<Photo> photos = new ArrayList<>();
+    private String type = "";
+    private String price = "";
+    private String location = "";
+    private String description = "";
+    private String surface = "";
+    private String rooms = "";
+    private String bathrooms = "";
+    private String address = "";
+    private boolean sold = false;
+    private String startDate = "";
+    private String endDate = "";
+    private String agent = "";
+    private String[] pointsOfInterest = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +92,7 @@ public class CreateRealEstateActivity extends AppCompatActivity implements Adapt
         ButterKnife.bind(this);
         initAnimation(savedInstanceState);
         initSpinner();
+        initButton();
     }
 
     private void initAnimation(Bundle savedInstanceState) {
@@ -86,24 +145,28 @@ public class CreateRealEstateActivity extends AppCompatActivity implements Adapt
     }
 
 
-    private void initSpinner(){
+    private void initSpinner() {
         //Rooms spinner
-        Spinner spinner = (Spinner) findViewById(R.id.spinner_rooms);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.rooms_array, android.R.layout.simple_spinner_item);
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
+        spinnerRooms.setAdapter(adapter);
+        spinnerRooms.setOnItemSelectedListener(this);
 
         //Bathrooms spinner
-         Spinner spinnerBathroom = (Spinner) findViewById(R.id.spinner_bathrooms);
         ArrayAdapter<CharSequence> adapterBathroom = ArrayAdapter.createFromResource(this,
                 R.array.bathrooms_array, android.R.layout.simple_spinner_item);
 
         adapterBathroom.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerBathroom.setAdapter(adapter);
-        spinnerBathroom.setOnItemSelectedListener(this);
+        spinnerBathrooms.setAdapter(adapter);
+        spinnerBathrooms.setOnItemSelectedListener(this);
+    }
+
+    private void initButton() {
+        btn_validate.setOnClickListener(v -> getInfoFromUI());
+        house.setOnClickListener(v -> getType("tag_house"));
+        apartement.setOnClickListener(v -> getType("tag_apartement"));
     }
 
     public void onItemSelected(AdapterView<?> parent, View view,
@@ -114,6 +177,28 @@ public class CreateRealEstateActivity extends AppCompatActivity implements Adapt
 
     public void onNothingSelected(AdapterView<?> parent) {
         // Another interface callback
+    }
+
+
+    private void getInfoFromUI() {
+        agent = agent_tv.getText().toString();
+        if (type.isEmpty()) {
+            type = type_tv.getText().toString();
+        }
+    }
+
+    private void getType(String tag) {
+        switch (tag) {
+            case "tag_house":
+                type = "house";
+                //CHANGE COLOR TO SHOW CLICKED
+                break;
+            case "tag_apartement":
+                type = "apartement";
+                break;
+            default:
+                type = "";
+        }
     }
 }
 
--- app/src/main/java/com/openclassrooms/realestatemanager/models/RealEstate.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/models/RealEstate.kt
@@ -6,14 +6,19 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class RealEstate(@PrimaryKey(autoGenerate = true) val id: Long? = null,
-                      var photo: String,
+                      var photos: List<Photo>,
                       var type: String,
                       var price: String,
                       var location: String,
-                      var description :String,
+                      var description: String,
                       var surface: String,
                       var bedrooms: String,
                       var rooms: String,
-                      var bathrooms:String,
-                      var address: String
-                      )
+                      var bathrooms: String,
+                      var address: String,
+                      var sold: Boolean,
+                      var startDate: String,
+                      var endDate: String,
+                      var agent: String,
+                      var pointsOfInterest: List<String>
+)
